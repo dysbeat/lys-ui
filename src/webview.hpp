f44a3cc4bc5440bec314b5d5f2d8be8978a883a9
vianney <vianney.plotton@gmail.com>
2018-08-05 12:25:58 +0200

    webview shall explicitily be private.

--- src/webview.hpp
+++ src/webview.hpp
@@ -23,35 +23,35 @@ struct view
     view(std::string title, std::string local_url)
         : _title{title}, _local_url{local_url}
     {
-        wv.title = _title.c_str();
+        _wv.title = _title.c_str();
         auto path{adapt_url(_local_url)};
-        wv.url = path.c_str();
+        _wv.url = path.c_str();
         std::cout << "adapted_url: " << path.c_str() << '\n';
-        wv.width = 800;
-        wv.height = 600;
-        wv.debug = /* is debug */ 1;
-        wv.resizable = /* is resizable */ 1;
+        _wv.width = 800;
+        _wv.height = 600;
+        _wv.debug = /* is debug */ 1;
+        _wv.resizable = /* is resizable */ 1;
 
-        webview_init(&wv);
+        webview_init(&_wv);
     }
 
     void run()
     {
         auto start = std::chrono::high_resolution_clock::now();
-        while (webview_loop(&wv, /*blocking */ 0) == 0)
+        while (webview_loop(&_wv, /*blocking */ 0) == 0)
         {
             auto stop = std::chrono::high_resolution_clock::now();
             if (stop - start > std::chrono::seconds{5})
             {
-                wv.title = "asdasdasd";
+                _wv.title = "asdasdasd";
             }
         }
 
-        webview_exit(&wv);
+        webview_exit(&_wv);
     }
 
   private:
-    struct ::webview wv;
+    struct ::webview _wv;
     std::string _title;
     std::string _local_url;
 };
